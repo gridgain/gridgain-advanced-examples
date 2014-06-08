e6d25cd66dc0fee72a42837ce85ac2b6ee74a9c7
Yakov Zhdanov <yzhdanov@gridgain.com>
2014-06-09 01:59:42 +0300

    wip

--- src/main/java/org/gridgain/examples/datagrid/near/NearCacheExample.java
+++ src/main/java/org/gridgain/examples/datagrid/near/NearCacheExample.java
@@ -14,8 +14,106 @@
 
 package org.gridgain.examples.datagrid.near;
 
+import org.gridgain.grid.*;
+import org.gridgain.grid.cache.*;
+import org.gridgain.grid.resources.*;
+
+import java.util.concurrent.*;
+
 /**
  *
  */
 public class NearCacheExample {
+    /** Cache name. */
+    private static final String CACHE_NAME = "partitioned";
+
+    /**
+     * Executes example.
+     *
+     * @param args Command line arguments, none required.
+     * @throws GridException If example execution failed.
+     */
+    public static void main(String[] args) throws Exception {
+        try (Grid g = GridGain.start("config/example-cache-near.xml")) {
+            if (g.nodes().size() == 1) {
+                System.err.println("Start remote nodes to run this example.");
+            }
+            else {
+                int key = -1;
+
+                for (int i = 0; i < 1000; i++) {
+                    GridNode n = g.mapKeyToNode(CACHE_NAME, i);
+
+                    if (!g.localNode().equals(n)) {
+                        key = i;
+
+                        break;
+                    }
+                }
+
+                if (key == -1)
+                    throw new Exception("Failed to map key to remote node.");
+
+                if (g.cache(CACHE_NAME).get(key) != null)
+                    throw new Exception("Key should not be in cache: " + key);
+
+                final int key0 = key;
+
+                g.forRemotes().compute().call(new Callable<Object>() {
+                    @GridInstanceResource
+                    private Grid g0;
+
+                    @Override public Object call() throws Exception {
+                        System.out.println("Putting example key on node: " + g.localNode().id());
+
+                        return g0.cache(CACHE_NAME).putx(key0, 10);
+                    }
+                }).get();
+
+                if (g.cache(CACHE_NAME).peek(key) != null)
+                    throw new Exception("Key should not be in cache: " + key);
+
+                // This will create near entry.
+                if (g.cache(CACHE_NAME).get(key) != 10)
+                    throw new Exception("Unexpected value in cache: " + key);
+
+                if (g.cache(CACHE_NAME).peek(key) != 10)
+                    throw new Exception("Unexpected value in cache: " + key);
+
+                g.forRemotes().compute().call(new Callable<Object>() {
+                    @GridInstanceResource
+                    private Grid g0;
+
+                    @Override public Object call() throws Exception {
+                        System.out.println("Updating example key on node: " + g.localNode().id());
+
+                        return g0.cache(CACHE_NAME).flagsOn(GridCacheFlag.SYNC_COMMIT).putx(key0, 15);
+                    }
+                }).get();
+
+                Object val = g.cache(CACHE_NAME).peek(key);
+
+                if (val != 15)
+                    throw new Exception("Unexpected value in cache: " + key + " " + val);
+
+                g.forRemotes().compute().call(new Callable<Object>() {
+                    @GridInstanceResource
+                    private Grid g0;
+
+                    @Override public Object call() throws Exception {
+                        System.out.println("Removing example key on node: " + g.localNode().id());
+
+                        return g0.cache(CACHE_NAME).remove(key0);
+                    }
+                }).get();
+
+                if (g.cache(CACHE_NAME).peek(key) != null)
+                    throw new Exception("Key should not be in cache: " + key);
+
+                // This will create near entry.
+                if (g.cache(CACHE_NAME).get(key) != null)
+                    throw new Exception("Unexpected value in cache: " + key);
+            }
+        }
+    }
 }
---
+++ src/main/java/org/gridgain/examples/datagrid/near/NearCacheExampleNodeStartup.java
@@ -0,0 +1,34 @@
+/*
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.gridgain.examples.datagrid.near;
+
+import org.gridgain.grid.*;
+
+import javax.swing.*;
+
+/**
+ *
+ */
+public class NearCacheExampleNodeStartup {
+    /**
+     * @param args Command line arguments, none required.
+     * @throws GridException If example execution failed.
+     */
+    public static void main(String[] args) throws Exception {
+        try (Grid g = GridGain.start("config/example-cache.xml")) {
+            JOptionPane.showMessageDialog(null, "Waiting for example node (press OK to stop this node).");
+        }
+    }
+}
