50c629eb9d20426bc0643d2dc9b282419d0f6478
Dmitriy Setrakyan <dsetrakyan@gridgain.com>
2014-06-16 19:54:48 -0700

    Updated cache store example.

--- src/main/java/org/gridgain/examples/datagrid/store/CacheStoreExample.java
+++ src/main/java/org/gridgain/examples/datagrid/store/CacheStoreExample.java
@@ -25,6 +25,9 @@ import org.gridgain.grid.*;
 import org.gridgain.grid.cache.*;
 import org.gridgain.grid.logger.*;
 
+import static org.gridgain.grid.cache.GridCacheTxConcurrency.*;
+import static org.gridgain.grid.cache.GridCacheTxIsolation.*;
+
 /**
  * Demonstrates cache store with and without write-through.
  *
@@ -46,6 +49,12 @@ public class CacheStoreExample {
         testStore(true);
     }
 
+    /**
+     * Tests store with write-behind either turned on or off.
+     *
+     * @param writeBehind Write-behind flag.
+     * @throws GridException If failed.
+     */
     private static void testStore(boolean writeBehind) throws GridException {
         log(">>>");
         log(">>> Testing store with write-behind=" + writeBehind);
@@ -58,6 +67,7 @@ public class CacheStoreExample {
         GridCacheConfiguration cc = new GridCacheConfiguration();
 
         cc.setName("test");
+        cc.setAtomicityMode(GridCacheAtomicityMode.TRANSACTIONAL);
         cc.setCacheMode(GridCacheMode.PARTITIONED);
         cc.setStore(new CacheMongoStore());
 
@@ -68,17 +78,70 @@ public class CacheStoreExample {
         c.setCacheConfiguration(cc);
 
         try (Grid g = GridGain.start(c)) {
-            GridCache<Long, Employee> cache = g.cache("test");
+            atomicExample(g);
+            transactionExample(g);
+            lockExample(g);
+        }
+    }
 
-            GridLogger log = g.log().getLogger(CacheStoreExample.class);
+    /**
+     * Atomic example.
+     *
+     * @param g Grid.
+     * @throws GridException If failed.
+     */
+    private static void atomicExample(Grid g) throws GridException {
+        log(">>>");
+        log(">>> Atomic example.");
+        log(">>>");
 
-            int cnt = 10;
+        GridCache<Long, Employee> cache = g.cache("test");
 
+        GridLogger log = g.log().getLogger(CacheStoreExample.class);
+
+        int cnt = 10;
+
+        for (long i = 1; i <= cnt; i++)
+            cache.putx(i, new Employee(i, "Name-" + i, i * 1000));
+
+        // Evict every other employee.
+        for (long i = 1; i <= cnt; i+= 2) {
+            cache.evict(i);
+
+            log(log, "Evicted key: " + i);
+        }
+
+        for (long i = 1; i <= cnt; i++)
+            log(log, "Peeked at [key=" + i + ", val=" + cache.peek(i) + ']');
+
+        for (long i = 1; i <= cnt; i++)
+            log(log, "Got [key=" + i + ", val=" + cache.get(i) + ']');
+    }
+
+    /**
+     * Transactional example which acquires pessimistic locks.
+     *
+     * @param g Grid.
+     * @throws GridException If failed.
+     */
+    private static void transactionExample(Grid g) throws GridException {
+        log(">>>");
+        log(">>> Transactional example.");
+        log(">>>");
+
+        GridCache<Long, Employee> cache = g.cache("test");
+
+        GridLogger log = g.log().getLogger(CacheStoreExample.class);
+
+        int cnt = 10;
+
+        try (GridCacheTx tx = cache.txStart(PESSIMISTIC, REPEATABLE_READ)) {
+            // This will acquire pessimistic lock on all accessed employees.
             for (long i = 1; i <= cnt; i++)
                 cache.putx(i, new Employee(i, "Name-" + i, i * 1000));
 
             // Evict every other employee.
-            for (long i = 1; i <= cnt; i+= 2) {
+            for (long i = 1; i <= cnt; i += 2) {
                 cache.evict(i);
 
                 log(log, "Evicted key: " + i);
@@ -89,6 +152,52 @@ public class CacheStoreExample {
 
             for (long i = 1; i <= cnt; i++)
                 log(log, "Got [key=" + i + ", val=" + cache.get(i) + ']');
+
+            // Commit and release locks.
+            tx.commit();
+        }
+    }
+
+    /**
+     * Lock example which acquires a lock on cache and performs all operations
+     * under a single lock.
+     *
+     * @param g Grid.
+     * @throws GridException If failed.
+     */
+    private static void lockExample(Grid g) throws GridException {
+        log(">>>");
+        log(">>> Lock example.");
+        log(">>>");
+
+        GridCache<Long, Employee> cache = g.cache("test");
+
+        GridLogger log = g.log().getLogger(CacheStoreExample.class);
+
+        int cnt = 10;
+
+        // Acquire a single lock.
+        cache.lock(/*key*/0L, /*timeout*/0L);
+
+        try {
+            for (long i = 1; i <= cnt; i++)
+                cache.putx(i, new Employee(i, "Name-" + i, i * 1000));
+
+            // Evict every other employee.
+            for (long i = 1; i <= cnt; i += 2) {
+                cache.evict(i);
+
+                log(log, "Evicted key: " + i);
+            }
+
+            for (long i = 1; i <= cnt; i++)
+                log(log, "Peeked at [key=" + i + ", val=" + cache.peek(i) + ']');
+
+            for (long i = 1; i <= cnt; i++)
+                log(log, "Got [key=" + i + ", val=" + cache.get(i) + ']');
+        }
+        finally {
+            cache.unlock(/*key*/0L);
         }
     }
 
